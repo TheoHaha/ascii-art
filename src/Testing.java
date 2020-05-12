c6a4a267f33218906812d235d99965f5c5459401
TheoHaha <theomichailidis@gmail.com>
2020-05-13 00:52:36 +0300

    Very basic GUI implementation

--- src/Testing.java
+++ src/Testing.java
@@ -3,44 +3,112 @@
 
 import image_to_ASCII.*;
 
-import java.io.File;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JButton;
 
 public class Testing {
+	
+	private static JFileChooser fc;
+	private static JButton saveBtn;
+	private static JLabel fileLabel;
+	
+	private static ImageFile selectedImg = null;
 	
 	private static final String[] imageExts = {
 		"gif","jpeg","jpg","png","tif","tiff"
 	};
 	
 	public static void main(String[] args) {
-		final JFileChooser fc = new JFileChooser();
-		fc.setFileFilter(new FileNameExtensionFilter("All images", imageExts));
-		for(String s : imageExts) {
-			fc.addChoosableFileFilter(new FileNameExtensionFilter("."+s, s));
-		}
+		JFrame frame = new JFrame();
+		frame.setBounds(0, 0, 300, 200);
+		frame.setLayout(new FlowLayout());
+		frame.setVisible(true);
 		
-		int returnVal = fc.showOpenDialog(new JPanel());
+		// the load button and its functionality
+		JButton loadBtn = new JButton("Load...");
+		loadBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				loadImage();
+				if(selectedImg != null) {
+					saveBtn.setEnabled(true);
+					fileLabel.setText("Loaded file: "+selectedImg.getFileName());
+				} else {
+					saveBtn.setEnabled(false);
+					fileLabel.setText("Loaded file: <none>");
+				}
+			}
+		});
 		
-		File file;
+		// the save button and its functionality
+		saveBtn = new JButton("Save...");
+		saveBtn.setEnabled(false);
+		saveBtn.setToolTipText("Load an image first!");
+		saveBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveResult(selectedImg);
+			}
+		});
 		
-		if(fc.getSelectedFile() != null && returnVal == JFileChooser.APPROVE_OPTION) {
-			file = fc.getSelectedFile();
-			
-			ImageFile image1 = new ImageFile(file);
-			System.out.println(image1.getFileName());
-			
-			fc.resetChoosableFileFilters();
-			fc.setFileFilter(new FileNameExtensionFilter(".txt", "txt"));
-			returnVal = fc.showSaveDialog(new JPanel());
-			
-			if(fc.getSelectedFile() != null && returnVal == JFileChooser.APPROVE_OPTION) {
-				file = fc.getSelectedFile();
-				
-				FileResult result1 = new FileResult(file.getPath()+".txt");
-				result1.printResult(image1.scale(0.5));
+		fileLabel = new JLabel("Loaded file: <none>");
+		
+		frame.add(loadBtn);
+		frame.add(fileLabel);
+		frame.add(saveBtn);
+	}
+	
+	public static void loadImage() {
+		// initialize the file chooser
+		fc = new JFileChooser();
+		
+		// set up the file filters
+		fc.resetChoosableFileFilters();
+		String s = "";
+		for(String ext : imageExts) {
+			fc.addChoosableFileFilter(new FileNameExtensionFilter(ext.toUpperCase(), ext));
+			if (ext != imageExts[imageExts.length - 1]) {
+				s += "."+ext+", ";
+			} else {
+				s += "."+ext;
 			}
 		}
+		fc.setFileFilter(new FileNameExtensionFilter("Images ("+s+")", imageExts));
+		
+		// open up the load window
+		int returnVal = fc.showOpenDialog(new JPanel());
+		
+		// return the selected file if the user chooses to load it
+		if(fc.getSelectedFile() != null && returnVal == JFileChooser.APPROVE_OPTION) {
+			selectedImg = new ImageFile(fc.getSelectedFile());
+		} else {
+			selectedImg = null;
+		}
+	}
+	
+	public static void saveResult(ImageFile image) {
+		// initialize the file chooser and create an ImageFile
+		fc = new JFileChooser();
+		
+		// set up the file filters
+		fc.resetChoosableFileFilters();
+		fc.setFileFilter(new FileNameExtensionFilter(".txt", "txt"));
+		
+		// open up the save window
+		int returnVal = fc.showSaveDialog(new JPanel());
+		
+		// save the result to the specified .txt file
+		if(fc.getSelectedFile() != null && returnVal == JFileChooser.APPROVE_OPTION) {
+			FileResult result1 = new FileResult(fc.getSelectedFile().getPath()+".txt");				
+			result1.printResult(image.scale(0.5));
+		}
+	
 	}
 }
