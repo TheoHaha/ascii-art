02db5c9ca74500d997b13dace99988f3417a25d3
TheoHaha <theomichailidis@gmail.com>
2020-05-12 18:27:15 +0300

    Even more tests, implementing file extension filters

--- src/Testing.java
+++ src/Testing.java
@@ -6,21 +6,42 @@ import image_to_ASCII.*;
 import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Testing {
 	
+	private static final String[] imageExts = {
+		"gif","jpeg","jpg","png","tif","tiff"
+	};
+	
 	public static void main(String[] args) {
 		final JFileChooser fc = new JFileChooser();
+		fc.setFileFilter(new FileNameExtensionFilter("All images", imageExts));
+		for(String s : imageExts) {
+			fc.addChoosableFileFilter(new FileNameExtensionFilter("."+s, s));
+		}
+		
+		@SuppressWarnings("unused")
 		int returnVal = fc.showOpenDialog(new JPanel());
 		
-		File file = fc.getSelectedFile();
+		File file;
 		
-		ImageFile image1 = new ImageFile(file);
-		System.out.println(image1.getFileName());
-		
-		int returnVal2 = fc.showSaveDialog(new JPanel());
-		
-		FileResult result1 = new FileResult(fc.getSelectedFile());
-		result1.printResult(image1);
+		if(fc.getSelectedFile() != null) {
+			file = fc.getSelectedFile();
+			
+			ImageFile image1 = new ImageFile(file);
+			System.out.println(image1.getFileName());
+			
+			fc.resetChoosableFileFilters();
+			fc.setFileFilter(new FileNameExtensionFilter(".txt", "txt"));
+			returnVal = fc.showSaveDialog(new JPanel());
+			
+			if(fc.getSelectedFile() != null) {
+				FileResult result1 = new FileResult(fc.getSelectedFile());
+				result1.printResult(image1);
+			}
+		}
 	}
 }
